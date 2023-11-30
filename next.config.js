0d853ca7e05ae7735be47847a03a8d54993aa2df
Abusantos <abusomwansantos@gmail.com>
2023-11-30 22:47:22 +0100

    updating vercel

--- next.config.js
+++ next.config.js
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  distDir: "_next",
-  generateBuildId: async () => {
-    if (process.env.BUILD_ID) {
-      return process.env.BUILD_ID;
-    } else {
-      return `${new Date().getTime()}`;
-    }
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
